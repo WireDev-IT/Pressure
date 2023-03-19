ee1d65bf91872daf4574150b9e9d14ed5cab5f03
tomo2403 <49612544+tomo2403@users.noreply.github.com>
2023-03-19 13:55:21 +0100

    added new dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.TimeZoneInfo;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Animator animator;
+    public TMP_Text title;
+    public TMP_Text message;
+    public Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DisplayNextSentence();
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        title.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentences.Dequeue()));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        message.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            message.text += letter;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+
+    private void EndDialog()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+}

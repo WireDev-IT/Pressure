bb98ff256912334bceb5e278198e472a937edf6b
Jxstxs <juleb0202@gmail.com>
2023-03-19 12:31:56 +0100

    backup before a risky operation

--- Assets/TriggerInteraction.cs
+++ Assets/TriggerInteraction.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Collider2D[] objDetected;
+    public Transform Position;
+    public float height;
+    public float width;
+    public LayerMask InteractionLayer;
+
+    public GameObject menu;
+
+    void Start() {
+        menu = GetComponent<GameObject>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        objDetected = Physics2D.OverlapBox(
+            Position.position,
+            new Vector2(width, height),
+            0,
+            InteractionLayer
+        );
+
+        foreach (Collider2D colider in objDetected) {
+            Debug.Log(collider.gameObject.name);
+        }
+        // Debug.Log(playerDetected);
+        // if (Input.GetKeyDown(KeyCode.E)) { SceneManager.LoadScene("informatics_house_1"); }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(Position.position, new Vector3(width, height, 1));
     }
 }
